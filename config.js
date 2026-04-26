dd470ac57bc1618ec4a87c88b6263d71a4da3a2e
DominicDev <s0l0estudi4r@gmail.com>
2026-04-26 23:13:22 +0200

    v0.0.2

---
+++ config.js
@@ -0,0 +1,23 @@
+export const nameBand = 'STONE TEMPLE PILOTS';
+export let nameSongs = [
+	"PLUSH", /*1*/
+	"INTERSTATE LOVE SONG", /*2*/
+	"VASOLINE", /*3*/
+	"BIG EMPTY", /*4*/
+	"SEX TYPE THING", /*5*/
+	"DEAD & BLOATED", /*6*/
+	"WICKED GARDEN", /*7*/
+	"CREEP", /*8*/
+	"STILL REMAINS", /*9*/
+	"SILVERGUN SUPERMAN", /*10*/
+	"TRIIPPIN' ON A HOLE IN A PAPER HEART", /*11*/
+	"DOWN", /*12*/
+	"HEAVEN & HOT RODS", /*13*/
+	"LADY PICTURE SHOW", /*14*/
+	"ATLANTA", /*15*/
+	"BIG BANG BABY", /*16*/
+	"NO MEMORY", /*17*/
+	"DUMB LOVE", /*18*/
+	"AND SO I KNOW", /*19*/
+	"LONG WAY HOME", /*20*/
+]
